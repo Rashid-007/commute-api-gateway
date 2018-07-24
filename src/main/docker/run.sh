3de898ece8423d4235b5ebbcc3c12b27321b07ae
Enayetullah Rasuli <Enayetullah.Rasuli@softwareag.com>
2018-07-24 08:30:23 +0200

    second commit

---
+++ src/main/docker/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+getPort() {
+    echo $1 | cut -d : -f 3 | xargs basename
+}
+
+echo "********************************************************"
+echo "Waiting for the service discovery to start on port $(getPort $COMMUTESERVICEDISCOVERY_PORT)"
+echo "********************************************************"
+while ! `nc -z commuteservicediscovery  $(getPort $COMMUTESERVICEDISCOVERY_PORT)`; do sleep 3; done
+echo "******* service discovery  has started"
+
+echo "********************************************************"
+echo "Starting Commute API Gateway"
+echo "********************************************************"
+java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
+     -Deureka.client.serviceUrl.defaultZone=$COMMUTESERVICEDISCOVERY_URI   \
+     -Dspring.profiles.active=$PROFILE                          \
+     -jar /usr/local/commute-api-gateway/*.jar
